e6219a52203a3a0842f14b30407bc7fc86d756da
Diego Alejandro Estrada Beltrán <86diegoestrada@gmail.com>
2020-12-07 18:21:46 -0500

    shop class Diego create file

---
+++ Project/ShopTableViewCell.swift
@@ -0,0 +1,31 @@
+//
+//  ShopTableViewCell.swift
+//  Project
+//
+//  Created by Diego Estrada on 2020-12-07.
+//  Copyright © 2020 Chao Liu. All rights reserved.
+//
+
+import UIKit
+
+class ShopTableViewCell: UITableViewCell {
+
+    
+    
+    var idProduct:Int
+    var chipsQuantity:Double
+    var userName:String
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
